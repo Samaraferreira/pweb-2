7ed1f105f4ab9d233817365991184e7a6b77c3da
Samara Ferreira <51099911+Samaraferreira@users.noreply.github.com>
2023-07-02 16:25:50 -0300

    Create fibonacci.php

---
+++ fibonacci.php
@@ -0,0 +1,15 @@
+<?php
+function Fibonacci($number){
+    if ($number == 0)
+        return 0;    
+    else if ($number == 1)
+        return 1;    
+    else
+        return (Fibonacci($number-1) +  Fibonacci($number-2));
+}
+
+$number = 10; // número de termos
+for ($counter = 0; $counter < $number; $counter++){  
+    echo Fibonacci($counter),' ';
+}
+?>
